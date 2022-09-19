f93a65e570685c6694f15eea587108fd22d3211d
imArashAlishiri <108857795+imArashAlishiri@users.noreply.github.com>
2022-09-19 19:55:20 +0430

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,32 @@
 const circle = document.querySelector('.circle');
+const ham = document.querySelector('.ham');
+const nav = document.querySelector('.nav');
+
 
 circle.addEventListener('click', () => {
-    window.location.href = '../../jp/ee.html';
-})
+    circle.classList.add('circle-animate');
+
+    setTimeout(() => {
+        window.location.href = '../../jp/ee.html';
+    }, 200)
+});
+
+ham.addEventListener('click', animateNav)
+
+
+
+
+
+
+
+
+
+function animateNav() {
+    if(nav.classList.contains('nav-animate')) {
+        nav.classList.remove('nav-animate');
+        ham.classList.remove('ham-animate');
+    } else {
+        nav.classList.add('nav-animate');
+        ham.classList.add('ham-animate');
+    }
+}
