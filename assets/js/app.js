5ab039fa721042e2a3447a1033f97cf2cc0ef381
imArashAlishiri <108857795+imArashAlishiri@users.noreply.github.com>
2022-09-21 00:10:16 +0430

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ const navButtons = document.querySelectorAll('.nav__btn')
 const sections = document.querySelectorAll('.section');
 const quizSection = document.querySelector('.quiz-section');
 
+
 const lettersObj = {
     a: 'あ',
     i: 'い',
@@ -35,13 +36,65 @@ navButtons.forEach(btn => {
 ham.addEventListener('click', animateNav)
 
 letters.forEach(letter => {
+    
     letter.addEventListener('click', e => {
+
         const dataset = e.target.dataset.letter;
         
-        hideSections();
+        goToAlphabet();
 
-        quizSection.classList.remove('hide');
-        quizSection.textContent = lettersObj[dataset];
+        quizSection.innerHTML = `
+            <div class="alphabet-quiz"> 
+                <div class="alphabet-quiz__box">
+                    <div class="alphabet-quiz__buttons">
+                        <button class="alphabet-quiz__btn back-btn">
+                            <img src="assets/images/back-svgrepo-com.svg" alt="">
+                        </button>
+                        <button class="alphabet-quiz__btn speaker-btn">
+                            <img src="assets/images/speaker-svgrepo-com.svg" alt="">
+                        </button>
+                    </div>
+
+                    <div class="alphabet-quiz__content">
+                        <p class="alphabet-quiz__jp-letter">${lettersObj[dataset]}</p>
+                        <p class="alphabet-quiz__en-letter hidden">${dataset}</p>
+                    </div>
+                    </div>
+
+                    <div class="alphabet-quiz__input">
+                        <input type="text" autofocus class="alphabet-quiz__input-box" id="input">
+                    </div>
+                </div>
+            </div>
+        `;
+
+        document.querySelector('.back-btn').addEventListener('click', () => {
+            goToAlphabet();
+        })
+
+        document.querySelector('.alphabet-quiz__input-box').addEventListener('keypress', e => {
+            if(e.key === 'Enter') {
+                if(e.target.value.toLowerCase() == dataset) {
+                    document.querySelector('.alphabet-quiz__en-letter').classList.remove('hidden');
+                    e.target.value = '';
+                    boxColor('green');
+
+                    setTimeout(() => {
+                        document.querySelector('.alphabet-quiz').remove();
+                        goToAlphabet();
+                        alphabetSection.classList.remove('hide');
+                    }, 1000);
+                } else {
+                    e.target.value = '';
+                    boxColor('red');
+                }
+            }
+        }); 
+
+        document.querySelector('.speaker-btn').addEventListener('click', (e) => {
+
+        })
+
     })
 })
 
@@ -56,13 +109,26 @@ function animateNav() {
     }
 }
 
-
-function hideSections() {
-    alphabetSection.classList.add('hide');
-}
-
 function hideSections() {
     sections.forEach(section => {
             if(!section.classList.contains('hide')) section.classList.add('hide');
     });
 }
+
+function goToAlphabet() {
+    hideSections();
+    quizSection.classList.remove('hide');
+}
+
+function boxColor(color) {
+    const box = document.querySelector('.alphabet-quiz__box');
+
+    box.style.backgroundColor = color;
+    setTimeout(() => {
+        box.style.backgroundColor = '';
+    }, 200);
+}
+
+
+//  word section 
+
